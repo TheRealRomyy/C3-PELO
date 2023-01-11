eac2a10a2a08f9d8219ef072e478a6accdd43c0e
Whire-Coder <romeo.pst44@gmail.com>
2023-01-11 23:45:57 +0100

    Léa Soir

--- gui.py
+++ gui.py
@@ -12,7 +12,7 @@ class GUI:
 		root = tk.Tk()
 
 		root.title("C3-PELO")
-		root.geometry('300x300+50+50')
+		root.geometry('500x500+50+50')
 
 		mute_photo = tk.PhotoImage(file="res/mute.png").subsample(10,10)
 		mute_btn = tk.Button(root, image=mute_photo, command= lambda: self.volume.SetMute(1, None))
@@ -28,23 +28,22 @@ class GUI:
 		update_btn = tk.Button(text="update",command=self.update_with_volume)
 		update_btn.pack()
 
-		pythagore_photo = tk.PhotoImage(file="res/pythagore.png").subsample(10,10)
-		pythagore_btn = tk.Button(root, image=pythagore_photo, command=lambda: Pythagore.main_pythagore(label))
-		pythagore_btn.pack()
-
 		input_windows = tk.Entry(root)
 		input_windows.pack()
 
 		label = tk.Label(root, text="Rien a dire")
 		label.pack()
 
-		send_button = tk.Button(root, text="Send", command=lambda: )
+		pythagore = Pythagore(label, input_windows)
+
+		pythagore_photo = tk.PhotoImage(file="res/pythagore.png").subsample(10,10)
+		pythagore_btn = tk.Button(root, image=pythagore_photo, command=lambda: pythagore.first_display())
+		pythagore_btn.pack()
+
+		send_button = tk.Button(root, text="Entre trait", command=lambda: pythagore.result())
 		send_button.pack()
 
 		root.mainloop()
 
 	def update_with_volume(self):
 		self.display_volume_label["text"] = get_volume(self.volume)
-
-	def pytha(self):
-		self.pythagore_result = input_windows.get()
--- utils/pythagore.py
+++ utils/pythagore.py
@@ -2,46 +2,54 @@
 from math import *
 
 class Pythagore:
-	def main_pythagore(label, result):
-		# Demander ce qu'on veut calculer
-		label["text"] = "Que voulez vous calculer ? \n \n1: L'hyphothènuse \n2: Un côté"
-		toCalc = result
+
+	def __init__(self, label, input_windows):
+		self.time = 0
+		self.ab = 0
+		self.bc = 0
+
+		self.label = label
+		self.input_windows = input_windows
+
+	def first_display(self):
+		self.label["text"] = "Que voulez vous calculer ? \n \n1: L'hyphothènuse \n2: Un côté"
+	def result(self):
+		self.time = self.time + 1
+		self.input_windows.delete(0, "end")
+
+		if(self.time == 1):
+			res = self.input_windows.get()
+			if res != "1" and res != "2":
+				self.label["text"] = "Erreur: Vous deviez marqué 2 ou 1 !"
+				exit()
+
+			if res == "1":
+				self.hyphothenuse(self.time)
+			# elif res == "2":
+			# 	self.angle(label)
 		
-		# Vérifier si toCalc n'est ni 2 ni 1
-		if toCalc != "1" and toCalc != "2":
-			label["text"] = "Erreur: Vous deviez marqué 2 ou 1 !"
-			exit()
+	def hyphotenuse(self, time):
+		if(time == 2):
+			self.label["text"] = "Veuillez donnez la longueur du premier côté ( AB )"
+			self.ab = self.input_windows.get()
+		elif(time == 3):
+			self.label["text"] = "Veuillez donnez la longeur du deuxième côté ( BC )"
+			self.bc = self.input_windows.get()
+		else:
+			self.label["text"] = "Bien, votre triangle ABC rectangle en B tel que AB = " + self.ab + " et BC = " + self.bc + " !"
 
-		# Si toCalc est 1 calculer l'hyphothènuse si c'est 2 calculer un angle
-		if toCalc == "1":
-			hyphothenuse(label)
-			
-		elif toCalc == "2":
-			angle(label)
+			# Calculer l'hyphothènuse
+			ac2 = float(self.bc)*float(self.bc) + float(self.ab)*float(self.ab)
+			ac2 = sqrt(ac2)
 
-
-	# Creer la fonction hyphothènuse
-	def hyphothenuse(label):
-		label["text"] = "Veuillez donnez la longueure du premier côté ( AB )"
-		ab = input()
-
-		label["text"] = "Veuillez donnez la longeure du deuxième côté ( BC )"
-		bc = input()
-
-		label["text"] = "Bien, votre triangle ABC rectangle en B tel que AB = " + ab + " et BC = " + bc + " !"
-
-		# Calculer l'hyphothènuse
-		ac2 = float(bc)*float(bc) + float(ab)*float(ab)
-		ac2 = sqrt(ac2)
-
-		label["text"] = "L'hyphothènuse ( AC ) = " + str(ac2)
+			self.label["text"] = "L'hyphothènuse ( AC ) = " + str(ac2)
 
 	# Creer la fonction angle
 	def angle(label):
-		label["text"] = "Veuillez donnez la longueure du premier côté ( AB )"
+		label["text"] = "Veuillez donnez la longueur du premier côté ( AB )"
 		ab = input()
 
-		label["text"] = "Veuillez donnez la longeure du deuxième côté ( l'Hyphothènuse )"
+		label["text"] = "Veuillez donnez la longeur du deuxième côté ( l'Hyphothènuse )"
 		hypo = input()
 
 		label["text"] = "Bien, votre triangle ABC rectangle en B tel que AB = " + ab + " et l'hyphothènuse = " + hypo + " !"
