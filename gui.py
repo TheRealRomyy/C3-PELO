03620d5249b7681a42a951abbb946fceb4978629
Whire-Coder <romeo.pst44@gmail.com>
2023-01-12 20:53:44 +0100

    Soir

--- gui.py
+++ gui.py
@@ -2,55 +2,71 @@ import tkinter as tk
 from utils.functions import *
 from utils.pythagore import Pythagore
 
-class GUI:
+class GUI(tk.Tk):
 	def __init__(self, volume):
+		tk.Tk.__init__(self)
 		self.volume = volume
-		self.display_volume_label = None
-		self.pythagore_result = None
+
+		self.title("C3-PELO")
+		self.geometry('500x500+50+50')
+
+		self.frame = tk.Frame(self)
+		self.frame.pack(fill="both", expand=True)
+
+		GUI.buildGUI(self)
 
 	def buildGUI(self):
-		root = tk.Tk()
-
-		root.title("C3-PELO")
-		root.geometry('500x500+50+50')
-
 		# Mute button
 		mute_photo = tk.PhotoImage(file="res/mute.png").subsample(10,10)
-		mute_btn = tk.Button(root, image=mute_photo, command= lambda: self.volume.SetMute(1, None))
-		mute_btn.pack()
+		mute_btn = tk.Button(self, image=mute_photo, command= lambda: self.volume.SetMute(1, None))
+		mute_btn.pack(in_=self.frame)
 
 		# Unmute button
 		unmute_photo = tk.PhotoImage(file="res/unmute.png").subsample(10,10)
-		unmute_btn = tk.Button(root, image=unmute_photo, command= lambda: self.volume.SetMute(0, None))
-		unmute_btn.pack()
+		unmute_btn = tk.Button(self, image=unmute_photo, command= lambda: self.volume.SetMute(0, None))
+		unmute_btn.pack(in_=self.frame)
 
 		self.display_volume_label = tk.Label(text=get_volume(self.volume))
-		self.display_volume_label.pack()
+		self.display_volume_label.pack(in_=self.frame)
 
 		# Update volume
 		update_btn = tk.Button(text="update",command=self.update_with_volume)
-		update_btn.pack()
-
-				# Input windows
-		input_windows = tk.Entry(root)
-		input_windows.pack()
-
-		# Label
-		label = tk.Label(root)
-		label.pack()
-
-		pythagore = Pythagore(label, input_windows)
+		update_btn.pack(in_=self.frame)
 
 		# Start pythagore button
 		pythagore_photo = tk.PhotoImage(file="res/pythagore.png").subsample(10,10)
-		pythagore_btn = tk.Button(root, image=pythagore_photo, command=lambda: pythagore.first_display())
-		pythagore_btn.pack()
+		pythagore_btn = tk.Button(self, image=pythagore_photo, command=lambda:self.pytha())
+		pythagore_btn.pack(in_=self.frame)
+
+		# Input windows
+		self.input_windows = tk.Entry(self)
+		self.input_windows.pack(in_=self.frame)
+		GUI.hide_elements(self, self.input_windows)
+
+		# Label
+		self.label = tk.Label(self)
+		self.label.pack(in_=self.frame)
+		GUI.hide_elements(self, self.label)
 
 		# Send button
-		send_button = tk.Button(root, text="Entre trait", command=lambda: pythagore.result())
-		send_button.pack()
+		self.send_button = tk.Button(self, text="Envoie Zeubi", command=lambda: Pythagore.main(self, self.input_windows.get()))
+		self.send_button.pack(in_=self.frame)
+		GUI.hide_elements(self, self.send_button)
 
-		root.mainloop()
+		self.mainloop()
+
+	def pytha(self):
+		self.label["text"] = "Que voulez vous calculer ? \n \n1: L'hyphothènuse \n2: Un côté"
+		GUI.show_elements(self, self.input_windows, self.send_button, self.label)
+
 
 	def update_with_volume(self):
 		self.display_volume_label["text"] = get_volume(self.volume)
+
+	def show_elements(self, *elements):
+		for element in elements:
+			element.lift(self.frame)
+
+	def hide_elements(self, *elements):
+		for element in elements:
+			element.lower(self.frame)
--- utils/pythagore.py
+++ utils/pythagore.py
@@ -1,7 +1,14 @@
 # Importer math
 from math import *
+from tkinter import *
+from threading import Thread
+import time
 
 class Pythagore:
+
+	GUI = None
+	TIME = 0
+	TYPE = 0
 
 	def __init__(self, label, input_windows):
 		self.time = 0
@@ -11,51 +18,79 @@ class Pythagore:
 		self.label = label
 		self.input_windows = input_windows
 
-	def first_display(self):
-		self.label["text"] = "Que voulez vous calculer ? \n \n1: L'hyphothènuse \n2: Un côté"
-	def result(self):
-		self.time = self.time + 1
-		self.input_windows.delete(0, "end")
-
-		if(self.time == 1):
-			res = self.input_windows.get()
-			if res != "1" and res != "2":
-				self.label["text"] = "Erreur: Vous deviez marqué 2 ou 1 !"
-				exit()
-
-			if res == "1":
-				self.hyphothenuse(self.time)
-			# elif res == "2":
-			# 	self.angle(label)
-		
-	def hyphotenuse(self, time):
-		if(time == 2):
-			self.label["text"] = "Veuillez donnez la longueur du premier côté ( AB )"
-			self.ab = self.input_windows.get()
-		elif(time == 3):
-			self.label["text"] = "Veuillez donnez la longeur du deuxième côté ( BC )"
-			self.bc = self.input_windows.get()
+	def main(gui, res):
+		Pythagore.GUI = gui
+		if(Pythagore.TIME == 0):
+			if(res != "1" and res != "2"):
+				gui.label["text"] = "Erreur: Vous devez marquer 2 ou 1 !"
+				gui.label["fg"] = "#FF0000"
+				gui.hide_elements(gui, gui.send_button)
+				gui.input_windows.delete(0, END)
+				Thread(target=Pythagore.re_appear).start()
+			elif res == "1":
+				Pythagore.TIME += 1
+				Pythagore.TYPE = 1
+				Pythagore.hyphotenuse(Pythagore.TIME)
+			else:
+				Pythagore.TIME += 1
+				Pythagore.TYPE = 2
+				Pythagore.angle(Pythagore.TIME)
 		else:
-			self.label["text"] = "Bien, votre triangle ABC rectangle en B tel que AB = " + self.ab + " et BC = " + self.bc + " !"
+			Pythagore.TIME += 1
+			if Pythagore.TYPE == 1:
+				Pythagore.hyphotenuse(Pythagore.TIME)
+			else:
+				Pythagore.angle(Pythagore.TIME)
+
+	def re_appear():
+		time.sleep(3)
+		Pythagore.GUI.show_elements(Pythagore.GUI, Pythagore.GUI.send_button)
+		Pythagore.GUI.label["fg"] = "#000000"
+		Pythagore.GUI.label["text"] = "Que voulez vous calculer ? \n \n1: L'hyphothènuse \n2: Un côté"
+
+	def hyphotenuse(time):
+		if(time == 1):
+			Pythagore.GUI.input_windows.delete(0, END)
+			Pythagore.GUI.label["text"] = "Veuillez donnez la longueur du premier côté ( AB )"
+		elif(time == 2):
+			Pythagore.ab = Pythagore.GUI.input_windows.get()
+			Pythagore.GUI.input_windows.delete(0, END)
+			Pythagore.GUI.label["text"] = "Veuillez donnez la longeur du deuxième côté ( BC )"
+		elif(time == 3):
+			Pythagore.bc = Pythagore.GUI.input_windows.get()
+			Pythagore.GUI.input_windows.delete(0, END)
+			Pythagore.GUI.label["text"] = "Bien, votre triangle ABC rectangle en B tel que AB = " + Pythagore.ab + " et BC = " + Pythagore.bc + " !"
+		elif(time == 4):
 
 			# Calculer l'hyphothènuse
-			ac2 = float(self.bc)*float(self.bc) + float(self.ab)*float(self.ab)
+			ac2 = float(Pythagore.bc)*float(Pythagore.bc) + float(Pythagore.ab)*float(Pythagore.ab)
 			ac2 = sqrt(ac2)
 
-			self.label["text"] = "L'hyphothènuse ( AC ) = " + str(ac2)
+			Pythagore.GUI.label["text"] = "L'hyphothènuse ( AC ) = " + str(ac2)
+			Pythagore.GUI.hide_elements(Pythagore.GUI, Pythagore.GUI.send_button, Pythagore.GUI.input_windows)
+			Pythagore.TIME = 0
+			Pythagore.TYPE = 0
 
 	# Creer la fonction angle
-	def angle(label):
-		label["text"] = "Veuillez donnez la longueur du premier côté ( AB )"
-		ab = input()
+	def angle(time):
+		if(time == 1):
+			Pythagore.GUI.label["text"] = "Veuillez donnez la longueur du premier côté ( AB )"
+			Pythagore.ab = Pythagore.GUI.input_windows.get()
+			Pythagore.GUI.input_windows.delete(0, END)
+		elif(time == 2):
+			Pythagore.GUI.label["text"] = "Veuillez donnez la longeur du deuxième côté ( l'Hypothènuse )"
+			Pythagore.hypo = Pythagore.GUI.input_windows.get()
+			Pythagore.GUI.input_windows.delete(0, END)
+		elif(time == 3):
+			Pythagore.GUI.label["text"] = "Bien, votre triangle ABC rectangle en B tel que AB = " + Pythagore.ab + " et l'hyphothènuse = " + Pythagore.hypo + " !"
+			Pythagore.GUI.input_windows.delete(0, END)
 
-		label["text"] = "Veuillez donnez la longeur du deuxième côté ( l'Hyphothènuse )"
-		hypo = input()
+			# Calculer l'angle
+			bc2 = float(Pythagore.hypo)*float(Pythagore.hypo) - float(Pythagore.ab)*float(Pythagore.ab)
+			bc2 = sqrt(bc2)
 
-		label["text"] = "Bien, votre triangle ABC rectangle en B tel que AB = " + ab + " et l'hyphothènuse = " + hypo + " !"
+			Pythagore.GUI.label["text"] = "L'angle ( BC ) = " + str(bc2)
+			Pythagore.GUI.hide_elements(Pythagore.GUI, Pythagore.GUI.send_button, Pythagore.GUI.input_windows)
+			Pythagore.TIME = 0
+			Pythagore.TYPE = 0
 
-		# Calculer l'angle
-		bc2 = float(hypo)*float(hypo) - float(ab)*float(ab)
-		bc2 = sqrt(bc2)
-
-		label["text"] = ("BC = " + str(bc2))
